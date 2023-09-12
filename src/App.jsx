dc1d185d3d5cd783c9aa2cb885b7dc55f76407d5
Rose <i.a.rose.bk@gmail.com>
2023-09-12 12:34:54 -0400

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Components/Home'
+// import Home from './Components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -39,7 +39,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Home />
+    
     </>
   )
 }
