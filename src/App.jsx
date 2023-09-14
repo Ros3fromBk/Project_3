21363f6987c6ac8fc28c0ea4ef216951527e6e98
Rose <i.a.rose.bk@gmail.com>
2023-09-14 10:39:49 -0400

    Updated NavBar n App.jsx h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 // import Home from './Components/Home'
 
 function App() {
@@ -27,14 +29,32 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>Welcome To ShelterHub</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <Router>
+
+        <NavBar />
+      
+        {/* <div>
+          <SearchBar onSearch={handleSearch} />
+        </div>
+     */}
+
+        <Routes>
+          {/* <Route path="/" element={searchResults.length > 0 ?  */}
+          {/* <VideoPage searchResults={searchResults} toggleFavorite={toggleFavorite} favoriteVideoIds={favoriteVideoIds} /> : <div></div>} /> */}
+          <Route path="/AboutMe" element={<AboutList />} />
+          <Route path="/video/:videoId" element={<ShowPage />} />
+          {/* <Route path="/favorites" element={<FavoritesPage favoriteVideoIds={favoriteVideoIds} searchResults={searchResults} />} /> */}
+        </Routes>
+      </Router>
+
       </div>
       
     
