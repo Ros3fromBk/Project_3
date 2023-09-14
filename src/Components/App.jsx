bc058965edf1c39ef53038597cf6d5ed1dee0db3
Rose <i.a.rose.bk@gmail.com>
2023-09-14 12:10:54 -0400

    components structure

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -22,14 +22,14 @@ function App() {
       
       apiKey
     </h1>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}
 
       <h1>Welcome To ShelterHub</h1>
 
@@ -40,7 +40,9 @@ function App() {
 
       <Router>
 
-        <NavBar />
+      <header>
+      <NavBar />
+        </header>  
       
         {/* <div>
           <SearchBar onSearch={handleSearch} />
