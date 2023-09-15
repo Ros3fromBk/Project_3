8f5c8676fde09a3fc05c1fc168c5fc08cf011f04
Rubby <rubenslombard@pursuit.org>
2023-09-15 11:32:32 -0400

    change in the folder name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import InfoPages from './Components/InfoPages'
+import InfoPages from './Components/ResultPages'
 
 import NavBar from './NavBar'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
